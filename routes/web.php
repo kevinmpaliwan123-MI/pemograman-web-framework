c9482c8ace3db11f137af6085541c8ac31592679
kevinmpaliwan123-MI <kevin.m.paliwan123@gmail.com>
2026-09-16 11:32:04 +0800

    memperbaiki masalah di folder routes/ web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\BookController;
-=======
->>>>>>> 0c914c26a0772b837ebed69c6dd4d7149e030f4f
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
+
 
 Route::get('/buku',[BookController::class,'index']);
 Route::get('/buku/{id}',[BookController::class,'show']);
-=======
->>>>>>> 0c914c26a0772b837ebed69c6dd4d7149e030f4f
